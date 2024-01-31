afe8a4826156cc66da70ca0c396a5c6e2c2f5046
Joaquin Fierro <joaquin.fierro@api-ux.com>
2024-01-31 14:38:50 -0300

    cambios en el div, pero no funciona aun

--- src/components/componentePadre.js
+++ src/components/componentePadre.js
@@ -12,45 +12,21 @@ import 'primereact/resources/primereact.min.css';
 
 const ComponentePadre = () => {
     return (
-    <div className='mt-8 mx-auto '>
 
-        <div className="sticky top-0 z-50 bg-white p-4">
-            <h1 className="text-center text-2xl font-semibold">GRAFICOS CON RECHART</h1>
-        </div>
-
-        <div className='p-8'>
-            <div className="grid grid-cols-3 gap-4 justify-stretch pb-2">
-                <div>
-                    <GraficoBarrasReChart/>
-                </div>
-                <div>
-                    <GraficoBarrasVersion2/>
-                </div>
-                <div>
-                    <GraficoBarrasVersion2/>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 pb-4 ">
-                <div className=''>
-                    <h1 className='text-center font-semibold'>3</h1>
-                    <GraficoTortaReChart/>
-                </div>
-                <div className=''>
-                    <h1 className='text-center font-semibold'>4</h1>
-                    <GraficoTortaVersion2/>
-                </div>
-            </div>
-        </div>
-        <div className="grid grid-cols-2 gap-10">
-           
-        </div>
-        <div className="grid grid-cols-2 gap-10">
-            <TablaTop10 />
-           
-        </div>
+    // <div>
+        <div class="flex flex-wrap" style={{maxWidth: '600px'}}>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px', minHeight: '100px' }}><GraficoBarrasReChart/></div>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px;', minHeight: '100px'}}><GraficoBarrasVersion2/></div>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px;', minHeight: '100px'}}><GraficoTortaVersion2/></div>
+        </div>  
         
-    </div>
+        /* <div class="flex flex-wrap" style={{maxWidth: '600px'}}>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px;', minHeight: '100px'}}><GraficoBarrasReChart/></div>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px;', minHeight: '100px'}}><GraficoBarrasVersion2/></div>
+            <div class="flex align-items-center justify-content-center bg-primary font-bold m-2 border-round" style={{ minWidth: '200px;', minHeight: '100px'}}><GraficoTortaVersion2/></div>
+        </div> */
+    /* </div> */
+
     )
 }
 
